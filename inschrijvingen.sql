830fc0eb1d977e57fb5c4b6f4d42845f2e84de82
matthijs1888 <matthijsverboon12@gmail.com>
2019-05-10 14:42:05 +0200

    nieuwe klassen aangemaakt

---
+++ inschrijvingen.sql
@@ -0,0 +1,5 @@
+INSERT INTO klassen
+(klas_code, slb_code, cohort, opleidings_code)
+VALUES ("lo9e-amo1", "brc01", 2019, "amo"),
+       ("lo9e-amo2", "rs001", 2019, "amo"),
+       ("lo9e-amo3", "bkr02", 2019, "amo");
