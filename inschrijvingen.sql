61c4bc215a9606fddab740bcfed4874052434f8c
matthijs1888 <matthijsverboon12@gmail.com>
2019-05-10 14:50:25 +0200

    nieuwe studenten

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -3,3 +3,10 @@ INSERT INTO klassen
 VALUES ("lo9e-amo1", "brc01", 2019, "amo"),
        ("lo9e-amo2", "rs001", 2019, "amo"),
        ("lo9e-amo3", "bkr02", 2019, "amo");
+
+INSERT INTO studenten
+(ov_nummer, achternaam, tussenvoegsel, voorletters, voornaam, geboortedatum, postcode, plaats, gewicht, lengte,  inschrijvings_datum)
+VALUES (90000, "Jong", "de", "MH", "Mick", "1993-07-27", "2221 nm", "Katwijk ZH", 80, 1.90,"2019-01-01"),
+       (90001, "Paul", "", "P", "Paul","1993-08-27", "2222 aa", "leiden", 80, 1.90,"2019-01-01"),
+        (90002, "test", "", "T", "test","1993-11-27", "2223 aa", "leiden", 80, 1.90,"2019-01-01"),
+        (90003, "Mie", "", "P", "Mie","1993-12-27", "2224 aa", "leiden", 80, 1.90,"2019-01-01");
